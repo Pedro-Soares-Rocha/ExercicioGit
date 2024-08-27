5252c6c531db5792d95c7dedb60d2407f40b9b98
Pedro-Soares-Rocha <pedrosoaresrocha2006@gmail.com>
2024-08-27 15:35:46 -0300

    Escolha do codigo simples para o trabalho

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Esse progama é um codigo para um jogo de quiz aleatorio, planejo oferecer
+5 temas com cada tema contendo 5 perguntos para isso pretendo
+usar um metodo case e um random number para escolher uma pergunta aleatoria entre os temas
+*/
+
+string escolha
