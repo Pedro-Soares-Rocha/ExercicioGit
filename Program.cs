f0df62d3a61313514bb83616d38fb9ecc7170eb1
Pedro-Soares-Rocha <pedrosoaresrocha2006@gmail.com>
2024-08-27 15:56:30 -0300

    Primeiro teste do codigo

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
 ﻿/*Esse progama é um codigo para um jogo de quiz aleatorio, planejo oferecer
-5 temas com cada tema contendo 5 perguntos para isso pretendo
-usar um metodo case e um random number para escolher uma pergunta aleatoria entre os temas
+5 perguntas para isso pretendo usar um metodo case e um random number para 
+escolher uma pergunta aleatoria 
 */
 
-string escolha
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+string verificador;
+int questao;
+
+Random rand = new Random();
+
+questao = rand.Next(1, 5);
+//apenas para testar se funciona
+questao = 1;
+
+switch (questao)
+    {
+    case 1:
+        Console.WriteLine("Qual é a capital da frança ?");
+        verificador = Console.ReadLine();
+        if(verificador.ToUpper() == "PARIS")
+        {
+            Console.WriteLine("Acertou");
+        }
+        else
+        {
+            Console.WriteLine("Errou!");
+        }
+        break;
+    }
