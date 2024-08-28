3c561c161d110ab1a9e2484b7def64b61dd3ed40
Pedro-Soares-Rocha <pedrosoaresrocha2006@gmail.com>
2024-08-28 15:06:08 -0300

    Pequenas alterações e polimento do codigo

--- Program.cs
+++ Program.cs
@@ -2,15 +2,13 @@
 5 perguntas para isso pretendo usar um metodo case e um random number para 
 escolher uma pergunta aleatoria 
 */
-
 using static System.Runtime.InteropServices.JavaScript.JSType;
-
-string verificador = "", respostaUsuario = "", pergunta = "", respostaCorreta = "";
+string respostaUsuario = "", pergunta = "", respostaCorreta = "";
 int questao;
 
 Random rand = new Random();
 
-questao = rand.Next(1, 5);
+questao = rand.Next(1,5);
 
 void CriadorPergunta(string pergunta, string respostaUsuario, string respostaCorreta)
 {
@@ -31,19 +29,15 @@ switch (questao)
     case 1:
         CriadorPergunta("Qual é a capital da França:", "", "Paris");
         break;
-    
     case 2:
         CriadorPergunta("Qual é o quadrado de 7 ?", "", "49");
         break;
-        
     case 3:
         CriadorPergunta("Qual é o maior pais do mundo?", "", "RÚSSIA");
         break;
-
     case 4: 
         CriadorPergunta("Buenos Aires é a capital de qual país?", "", "ARGENTINA");
         break;
-    
     case 5:
         CriadorPergunta("Qual é a capital do Japão?", "", "TÓQUIO");
         break;
